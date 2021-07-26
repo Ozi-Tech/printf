9bc9b7c5d9d2b862dc4b5ea92bfd429898cf804d
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-07-26 12:08:37 +0100

    Initial files

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _printf - produces output according to a set format
+ * @format: character string
+ * Return: the number of characters printed (excluding NULL)
+ */
+
+int _printf(const char *format, ...)
+{
+    
+}
